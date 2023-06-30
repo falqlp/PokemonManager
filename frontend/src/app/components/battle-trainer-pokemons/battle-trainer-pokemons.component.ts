11756b44e50829911179892fe117d48dc3dd1cd0
Popole <falqleopaul@gmail.com>
2023-07-01 00:28:07 +0100

    cooldown

--- frontend/src/app/components/battle-trainer-pokemons/battle-trainer-pokemons.component.ts
+++ frontend/src/app/components/battle-trainer-pokemons/battle-trainer-pokemons.component.ts
@@ -12,8 +12,20 @@ export class BattleTrainerPokemonsComponent {
   @Input() public trainer: TrainerModel;
   @Input() public trainerPokemons: PokemonModel[];
   @Output() public clickOnPokemon = new EventEmitter<PokemonModel>();
+  protected disabled = false;
+  protected progress = 0;
 
   protected click(pokemon: PokemonModel): void {
     this.clickOnPokemon.emit(pokemon);
+    this.disabled = true;
+    this.progress = 100;
+    const interval = setInterval(() => {
+      this.progress -= 1; // ajustez cette valeur en fonction de la durée du cooldown
+      if (this.progress <= 0) {
+        clearInterval(interval);
+        this.disabled = false;
+        this.progress = 0;
+      }
+    }, 50); // ajustez cette valeur en fonction de la durée du cooldown
   }
 }
