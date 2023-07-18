17708475b6ec8bd450a794d335faae544e2cb72e
Popole <falqleopaul@gmail.com>
2023-07-18 21:59:24 +0100

    fichier couleur + composant affichage image pokemon

---
+++ frontend/src/app/components/display-pokemon-image/display-pokemon-image.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PokemonModel } from '../../models/PokemonModels/pokemon.model';
+import { DisplayType } from './display-pokemon-image.model';
+
+@Component({
+  selector: 'app-display-pokemon-image',
+  templateUrl: './display-pokemon-image.component.html',
+  styleUrls: ['./display-pokemon-image.component.scss'],
+})
+export class DisplayPokemonImageComponent implements OnInit {
+  @Input() public pokemon: PokemonModel;
+  @Input() public type: DisplayType;
+
+  protected imageUrl: string;
+
+  public ngOnInit(): void {
+    switch (this.type) {
+      case 'icon':
+        this.imageUrl = `assets/images/sprites/${this.pokemon.basePokemon.id}MS.png`;
+        break;
+      case 'back':
+        this.imageUrl = `assets/images/back/${this.pokemon.basePokemon.id}.png`;
+        break;
+      case 'thumbnails':
+        this.imageUrl = `assets/images/thumbnails/${this.pokemon.basePokemon.id}.png`;
+        break;
+    }
+  }
+}
