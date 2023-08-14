ae0693e51f9f6ceaf7c347793858a4406cb5f42a
Popole <falqleopaul@gmail.com>
2023-08-14 22:09:46 +0100

    swiper

--- frontend/src/app/components/pokemon-resume/pokemon-resume.component.ts
+++ frontend/src/app/components/pokemon-resume/pokemon-resume.component.ts
@@ -1,20 +1,11 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PokemonModel } from '../../models/PokemonModels/pokemon.model';
-import { Swiper } from 'swiper';
 
 @Component({
   selector: 'app-pokemon-resume',
   templateUrl: './pokemon-resume.component.html',
   styleUrls: ['./pokemon-resume.component.scss'],
 })
-export class PokemonResumeComponent implements AfterViewInit {
-  @ViewChild('swiperRef') public swiperContainer: HTMLElement;
-
+export class PokemonResumeComponent {
   @Input() public pokemon: PokemonModel;
-  protected swiper: Swiper;
-
-  public ngAfterViewInit(): void {
-    console.log(this.swiperContainer);
-    this.swiper = new Swiper(this.swiperContainer, {});
-  }
 }
