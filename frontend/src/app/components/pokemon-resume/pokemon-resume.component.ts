cbbcd77809fda960b3c1c86c844e3f9e9c95b59c
Popole <falqleopaul@gmail.com>
2023-08-17 22:48:08 +0100

    swiper

--- frontend/src/app/components/pokemon-resume/pokemon-resume.component.ts
+++ frontend/src/app/components/pokemon-resume/pokemon-resume.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { PokemonModel } from '../../models/PokemonModels/pokemon.model';
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-pokemon-resume',
   templateUrl: './pokemon-resume.component.html',
   styleUrls: ['./pokemon-resume.component.scss'],
 })
-export class PokemonResumeComponent {
+export class PokemonResumeComponent implements AfterViewInit {
+  @ViewChild('swiperContainer') protected swiperContainer: ElementRef;
+  protected swiper = Swiper;
   @Input()
   public set pokemon(value: PokemonModel) {
     this._pokemon = value;
@@ -17,4 +27,16 @@ export class PokemonResumeComponent {
   }
 
   protected _pokemon: PokemonModel;
+
+  public ngAfterViewInit(): void {
+    const swiperOption: SwiperOptions = {
+      pagination: {
+        clickable: true,
+      },
+    };
+    const swiperContainerEl = this.swiperContainer.nativeElement;
+    Object.assign(swiperContainerEl, swiperOption);
+    swiperContainerEl.initialize();
+    this.swiper = swiperContainerEl.swiper;
+  }
 }
