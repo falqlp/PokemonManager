861bb654fe98b03227b6d00fd0ff0cfdc72c89ab
Popole <falqleopaul@gmail.com>
2024-03-25 21:41:34 +0100

    test vercel

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -3,6 +3,16 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./app.ts",
+  externals: {
+    kerberos: "commonjs kerberos",
+    "@mongodb-js/zstd": "commonjs @mongodb-js/zstd",
+    "@aws-sdk/credential-providers": "commonjs @aws-sdk/credential-providers",
+    snappy: "commonjs snappy",
+    aws4: "commonjs aws4",
+    "mongodb-client-encryption": "commonjs mongodb-client-encryption",
+    bufferutil: "commonjs bufferutil",
+    "utf-8-validate": "commonjs utf-8-validate",
+  },
   module: {
     rules: [
       {
