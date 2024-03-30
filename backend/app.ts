463de7806462c58d84cd8fcb698e4ae1d69850ee
Popole <falqleopaul@gmail.com>
2024-03-30 14:45:12 +0100

    routesMap

--- backend/app.ts
+++ backend/app.ts
@@ -1,26 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import loginRoutes from "./routes/login";
-import pokemonBaseRoutes from "./api/pokemonBase/PokemonBaseRoute";
-import pokemonRoutes from "./api/pokemon/PokemonRoute";
-import trainerRoutes from "./api/trainer/TrainerRoute";
-import moveRoute from "./api/move/MoveRoute";
 import i18nService from "./i18n.service";
-import battleInstanceRoutes from "./api/battle-instance/BattleInstanceRoute";
-import battleRoutes from "./api/battle/BattleRoute";
-import moveLearningRoutes from "./api/moveLearning/MoveLearningRoutes";
-import pcStorageRoutes from "./api/pcStorage/PcStorageRoute";
-import calendarEventRoutes from "./api/calendar-event/CalendarEventRoutes";
-import gameRoutes from "./api/game/GameRoutes";
-import experienceRoutes from "./api/experience/ExperienceRoutes";
-import userRoutes from "./api/user/UserRoutes";
-import nurseryRoutes from "./api/nursery/NurseryRoutes";
-import TrainerService from "./api/trainer/TrainerService";
-import { ITrainer } from "./api/trainer/Trainer";
-import pokedexRouter from "./api/pokedex/PokedexRouter";
 import dotenv from "dotenv";
 import "source-map-support/register";
+import { RoutesMap } from "./RoutesMap";
 dotenv.config();
 
 const app = express();
@@ -60,20 +44,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/pokemonBase", pokemonBaseRoutes);
-app.use("/api/pokemon", pokemonRoutes);
-app.use("/api/trainer", trainerRoutes);
-app.use("/api/login", loginRoutes);
-app.use("/api/move", moveRoute);
-app.use("/api/battleInstance", battleInstanceRoutes);
-app.use("/api/battle", battleRoutes);
-app.use("/api/moveLearning", moveLearningRoutes);
-app.use("/api/pcStorage", pcStorageRoutes);
-app.use("/api/calendar-event", calendarEventRoutes);
-app.use("/api/game", gameRoutes);
-app.use("/api/xp", experienceRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/nursery", nurseryRoutes);
-app.use("/api/pokedex", pokedexRouter);
-
+for (const routesKey in RoutesMap) {
+  app.use(`/api/${routesKey}`, RoutesMap[routesKey]);
+}
 export default app;
