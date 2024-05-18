2b5ede6de4a489abcbdfea1c2622ba62ad827977
Popole <falqleopaul@gmail.com>
2024-05-18 11:15:45 +0200

    merge

--- frontend/src/app/services/player.service.ts
+++ frontend/src/app/services/player.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import type { TrainerModel } from '../models/TrainersModels/trainer.model';
-import { EMPTY, first, map, Observable, switchMap } from 'rxjs';
-import { BehaviorSubject, of, tap } from 'rxjs';
+import { EMPTY, map, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { PcStorageQueriesService } from './queries/pc-storage-queries.service';
 import { CacheService } from './cache.service';
 import { WebsocketEventService } from './websocket-event.service';
