327dce57ae623ae433a8f2adfbeaa4e3bd7b66c2
Léo-Paul Falq <leo-paul.falq@astree-software.fr>
2024-07-12 17:48:40 +0200

    delete create

--- backend/api/CompleteRouter.ts
+++ backend/api/CompleteRouter.ts
@@ -4,25 +4,12 @@ import { MongoId } from "../domain/MongoId";
 import { IMapper } from "../domain/IMapper";
 
 class CompleteRouter<T extends MongoId> extends ReadOnlyRouter<T> {
-  constructor(
-    protected service: CompleteRepository<T>,
-    mapper: IMapper<T>,
-  ) {
+  constructor(protected service: CompleteRepository<T>, mapper: IMapper<T>) {
     super(service, mapper);
     this.initCompleteRouter();
   }
 
   public initCompleteRouter(): void {
-    this.router.post("/", async (req, res, next) => {
-      try {
-        req.body.gameId = req.headers["game-id"] as string;
-        const obj = await this.service.create(req.body);
-        res.status(200).json(this.mapper.map(obj));
-      } catch (error) {
-        next(error);
-      }
-    });
-
     this.router.put("/:id", async (req, res, next) => {
       try {
         const obj = await this.service.update(req.params.id, req.body);
