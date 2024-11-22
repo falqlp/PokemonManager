5a21eb498b1608955c99b8f575cc7b0aebd31c5d
Léo-Paul <falqleopaul@gmail.com>
2024-11-22 17:45:47 +0100

    delete data service

--- backend/battle-websocket/src/app.module.ts
+++ backend/battle-websocket/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BattleWebsocketGateway } from './application/battle-websocket/battle-websocket.gateway';
 import { BattleWebsocketController } from './api/battle-websocket/battle-websocket.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, BattleWebsocketController],
-  providers: [AppService, BattleWebsocketGateway],
+  controllers: [BattleWebsocketController],
+  providers: [BattleWebsocketGateway],
 })
 export class AppModule {}
