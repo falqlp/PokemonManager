89904a0db9580a3f470c30da2aa5f2a65b2dbc90
Popole <falqleopaul@gmail.com>
2024-11-29 19:05:36 +0100

    [PM-95] news 1.2.0

---
+++ frontend/src/app/components/news/news-v1-2/news-v1-2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'pm-news-v1-2',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './news-v1-2.component.html',
+  styleUrl: './news-v1-2.component.scss',
+})
+export class NewsV12Component {}
--- frontend/src/app/views/main/main-page/main-page.component.ts
+++ frontend/src/app/views/main/main-page/main-page.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { NewsV11Component } from '../../../components/news/news-v1-1/news-v1-1.component';
+import { NewsV12Component } from '../../../components/news/news-v1-2/news-v1-2.component';
+import { MatDialog } from '@angular/material/dialog';
+import { NewsDialogComponent } from '../../../modals/news-dialog/news-dialog.component';
 
 @Component({
   selector: 'pm-main-page',
   standalone: true,
   imports: [
     MatButton,
-    NgOptimizedImage,
     RouterLink,
     TranslateModule,
     NewsV11Component,
+    NewsV12Component,
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+  constructor(protected readonly matDialog: MatDialog) {
+    matDialog.open(NewsDialogComponent);
+  }
+}
