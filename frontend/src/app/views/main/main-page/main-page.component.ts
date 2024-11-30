968619cd845c9769250e34b7fdfb1c0662b76224
Léo-Paul <falqleopaul@gmail.com>
2024-11-30 13:12:18 +0100

    fin

--- frontend/src/app/views/main/main-page/main-page.component.ts
+++ frontend/src/app/views/main/main-page/main-page.component.ts
@@ -4,8 +4,6 @@ import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { NewsV11Component } from '../../../components/news/news-v1-1/news-v1-1.component';
 import { NewsV12Component } from '../../../components/news/news-v1-2/news-v1-2.component';
-import { MatDialog } from '@angular/material/dialog';
-import { NewsDialogComponent } from '../../../modals/news-dialog/news-dialog.component';
 
 @Component({
   selector: 'pm-main-page',
@@ -20,8 +18,4 @@ import { NewsDialogComponent } from '../../../modals/news-dialog/news-dialog.com
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-  constructor(protected readonly matDialog: MatDialog) {
-    matDialog.open(NewsDialogComponent);
-  }
-}
+export class MainPageComponent {}
